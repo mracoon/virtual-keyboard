9676914e2fcb2692f837d1fc4b95ccbfa9d01104
mracoon <nataliyakomarova777@gmail.com>
2023-04-16 22:02:39 +0300

    feat: add eslint(eslint-config-airbnb-base)

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': 'airbnb-base',
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
