042a6633014c9eface52db19200468142ca4389e
mracoon <nataliyakomarova777@gmail.com>
2023-04-18 21:08:55 +0300

    feat: add base structure

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// import './style.css';
+// -----------create base structure---------------
+
+const body = document.querySelector('body');
+
+const container = document.createElement('div');
+container.classList.add('container');
+
+const textField = document.createElement('textarea');
+textField.autofocus = true;
+// textField.cols = 20;
+// textField.rows = 10;
+textField.value = '';
+
+container.append(textField);
+
+const keyboard = document.createElement('div');
+keyboard.classList.add('keyboard__container');
+container.append(keyboard);
+
+// ---
+body.append(container);
+
+// -----------------------------------------------------
