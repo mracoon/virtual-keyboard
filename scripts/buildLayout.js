334b2844e30623e036b3435f089c0cfd70cb94ab
mracoon <nataliyakomarova777@gmail.com>
2023-04-26 21:34:23 +0300

    feat: update text field layout

--- scripts/buildLayout.js
+++ scripts/buildLayout.js
@@ -2,15 +2,69 @@ const buildLayout = () => {
   const bodyElem = document.querySelector('body');
   const containerElem = document.createElement('div');
 
+  // *******************notepad start****************
+  const notepad = document.createElement('div');
+  notepad.classList.add('notepad');
+  // --------------notepadHeader start------------------
+  const notepadHeader = document.createElement('div');
+  notepadHeader.classList.add('notepad__header');
+  notepadHeader.textContent = 'Untitled - Notepad';
+  // --------------notepadBtns start------------------
+  const notepadBtnsContainer = document.createElement('div');
+  notepadBtnsContainer.classList.add('notepad__btns-container');
+
+  const notepadBtns = [['minimize', '🗕'], ['maximize', '🗖'], ['close', '🗙']];
+
+  notepadBtns.forEach((el) => {
+    const [btnClass, content] = el;
+    const notepadBtn = document.createElement('div');
+    notepadBtn.classList.add('notepad__btn', btnClass);
+    notepadBtn.textContent = content;
+    notepadBtnsContainer.append(notepadBtn);
+  });
+  // --------------notepadBtns end------------------
+
+  notepadHeader.append(notepadBtnsContainer);
+  notepad.append(notepadHeader);
+  // -----------------notepadHeader end------------------------------
+
+  // ----------------------notepadMenu start--------------------------------------
+  const notepadMenu = document.createElement('div');
+  notepadMenu.classList.add('notepad__menu');
+  // -------------------menu-list start--------------------
+  const menuList = document.createElement('ul');
+  menuList.classList.add('menu-list');
+
+  const notepadMenuItems = ['File', 'Edit', 'Format', 'View', 'Help'];
+
+  notepadMenuItems.forEach((item) => {
+    const menuItem = document.createElement('li');
+    menuItem.classList.add('menu-item');
+    menuItem.textContent = item;
+    menuList.append(menuItem);
+  });
+
+  // -------------------menu-list end--------------------
+  notepadMenu.append(menuList);
+  notepad.append(notepadMenu);
+  // -----------------notepadMenu end--------------------
+  // --------------------textarea start-------------------------
   const textFieldElem = document.createElement('textarea');
-  const keyboardElem = document.createElement('div');
-  containerElem.classList.add('container');
+  textFieldElem.classList.add('textarea');
+
   textFieldElem.autofocus = true;
   // textField.cols = 20;
   textFieldElem.rows = 10;
   textFieldElem.value = '';
 
-  containerElem.append(textFieldElem);
+  notepad.append(textFieldElem);
+  // --------------------textarea end-------------------------
+
+  containerElem.append(notepad);
+  // ********************notepad end********************
+  // **********************keyboard start*******************************
+  const keyboardElem = document.createElement('div');
+  containerElem.classList.add('container');
 
   keyboardElem.classList.add('keyboard__container');
 
@@ -23,7 +77,7 @@ const buildLayout = () => {
   }
 
   containerElem.append(keyboardElem);
-
+  // **********************keyboard end*******************************
   bodyElem.append(containerElem);
 };
 
