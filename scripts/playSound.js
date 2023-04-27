32d254d4a52b999208c1686965e5280129c6f3d0
mracoon <nataliyakomarova777@gmail.com>
2023-04-27 22:00:19 +0300

    fix: fix audio src

--- scripts/playSound.js
+++ scripts/playSound.js
@@ -1,7 +1,6 @@
 function soundPlay(isPlay) {
   if (isPlay) {
-    const audio = new Audio();
-    audio.src = '../assets/key-down.mp3';
+    const audio = new Audio('./assets/key-down.mp3');
     audio.play();
   }
 }
